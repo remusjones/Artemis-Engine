74db59e86d12e78de3aeda4d06e290637d3e5689
remus.jones <remus.jones@hotmail.com>
2023-12-28 16:05:44 +1100

    Replaces bat compilation with cmake custom commands

--- CompileShaders.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set VULKAN_SDK=%VULKAN_SDK%
-set OUTPUT_FOLDER=cmake-build-debug/Shaders
-if not exist "%OUTPUT_FOLDER%" md "%OUTPUT_FOLDER%"
-@echo on
-for /r %%i in (*.frag, *.vert) do %VULKAN_SDK%\Bin\glslc %%i -o %OUTPUT_FOLDER%/%%~ni.spv
