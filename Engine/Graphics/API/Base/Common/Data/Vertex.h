f4d65ee91651a75e1054ae09999a5691bc900d52
remus.jones <remus.jones@hotmail.com>
2024-01-27 13:58:12 +1100

    Improves Vertex bindings by utilizing masking

--- Engine/Graphics/API/Base/Common/Data/Vertex.h
+++ Engine/Graphics/API/Base/Common/Data/Vertex.h
@@ -4,9 +4,19 @@
 
 #pragma once
 
+#include <bitset>
 #include <vulkan/vulkan_core.h>
 #include "glm/glm.hpp"
 #include <vector>
+
+enum class VertexAttributeBinding : uint32_t {
+    Position = 0,
+    Normal,
+    Color,
+    UV,
+    Tangent
+};
+
 
 class alignas(16) Vertex {
 public:
@@ -53,31 +63,52 @@ public:
         attributeDescriptions[4].offset = offsetof(Vertex, mTangent);
         return attributeDescriptions;
     }
-};
 
-class Position
-{
-public:
-    glm::vec3 mPosition{};
+    static std::vector<VkVertexInputAttributeDescription>
+      GetAttributeDescriptions(std::bitset<5> bindingMask) {
+        std::vector<VkVertexInputAttributeDescription> attributeDescriptions;
+        attributeDescriptions.reserve(bindingMask.count());
 
-    Position() = default;
-    explicit Position(const glm::vec3 position) {
-        mPosition = position;
-    }
+        auto addAttributeDescription = [&attributeDescriptions, bindingMask](
+            VertexAttributeBinding binding, uint32_t location, VkFormat format, size_t offset) {
+            if (bindingMask[static_cast<size_t>(binding)]) {
+                VkVertexInputAttributeDescription attributeDescription{};
+                attributeDescription.binding = 0;
+                attributeDescription.location = location;
+                attributeDescription.format = format;
+                attributeDescription.offset = static_cast<uint32_t>(offset);
+                attributeDescriptions.push_back(attributeDescription);
+            }
+        };
 
-    static VkVertexInputBindingDescription GetBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Position);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return bindingDescription;
-    }
-    static std::vector<VkVertexInputAttributeDescription> GetAttributeDescriptions() {
-        std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Position, mPosition);
+        addAttributeDescription(VertexAttributeBinding::Position, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mPosition));
+        addAttributeDescription(VertexAttributeBinding::Normal, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mNormal));
+        addAttributeDescription(VertexAttributeBinding::Color, 2, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mColor));
+        addAttributeDescription(VertexAttributeBinding::UV, 3, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mUV));
+        addAttributeDescription(VertexAttributeBinding::Tangent, 4, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mTangent));
+
         return attributeDescriptions;
+    }
+
+    [[nodiscard]] static std::bitset<5> GetBindingMask(
+        const bool position = true,
+        const bool normal = true,
+        const bool color = true,
+        const bool uv = true,
+        const bool tangent = true) {
+        std::bitset<5> bindingMask;
+
+        if (position)
+            bindingMask.set(static_cast<size_t>(VertexAttributeBinding::Position));
+        if (normal)
+            bindingMask.set(static_cast<size_t>(VertexAttributeBinding::Normal));
+        if (color)
+            bindingMask.set(static_cast<size_t>(VertexAttributeBinding::Color));
+        if (uv)
+            bindingMask.set(static_cast<size_t>(VertexAttributeBinding::UV));
+        if (tangent)
+            bindingMask.set(static_cast<size_t>(VertexAttributeBinding::Tangent));
+
+        return bindingMask;
     }
 };
